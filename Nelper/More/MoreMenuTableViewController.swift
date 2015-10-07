99ecbfe33771c54d59641fbf79b65c1357003eb4
Charles Vinette <vinettecharles@gmail.com>
2015-10-07 15:09:59 -0400

    no message

---
+++ Nelper/More/MoreMenuTableViewController.swift
@@ -0,0 +1,80 @@
+//
+//  MoreMenuTableViewController.swift
+//  Nelper
+//
+//  Created by Charles Vinette on 2015-10-06.
+//  Copyright © 2015 Nelper. All rights reserved.
+//
+
+import Foundation
+
+class MoreMenuTableViewController : UITableViewController{
+	var selectedMenuItem : Int = 0
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// Customize apperance of table view
+		tableView.contentInset = UIEdgeInsetsMake(64.0, 0, 0, 0) //
+		tableView.separatorStyle = .None
+		tableView.backgroundColor = UIColor.clearColor()
+		tableView.scrollsToTop = false
+		
+		// Preserve selection between presentations
+		self.clearsSelectionOnViewWillAppear = false
+		
+		tableView.selectRowAtIndexPath(NSIndexPath(forRow: selectedMenuItem, inSection: 0), animated: false, scrollPosition: .Middle)
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	// MARK: - Table view data source
+	
+	override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+		// Return the number of sections.
+		return 1
+	}
+	
+	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		// Return the number of rows in the section.
+		return 4
+	}
+	
+	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		
+		var cell = tableView.dequeueReusableCellWithIdentifier("CELL")
+		
+		if (cell == nil) {
+			cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "CELL")
+			cell!.backgroundColor = UIColor.clearColor()
+			cell!.textLabel?.textColor = UIColor.darkGrayColor()
+			let selectedBackgroundView = UIView(frame: CGRectMake(0, 0, cell!.frame.size.width, cell!.frame.size.height))
+			selectedBackgroundView.backgroundColor = UIColor.grayColor().colorWithAlphaComponent(0.2)
+			cell!.selectedBackgroundView = selectedBackgroundView
+		}
+		
+		cell!.textLabel?.text = "ViewController #\(indexPath.row+1)"
+		
+		return cell!
+	}
+	
+	override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+		return 50.0
+	}
+	
+	override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+		
+		print("did select row: \(indexPath.row)")
+		
+		if (indexPath.row == selectedMenuItem) {
+			return
+		}
+		
+		selectedMenuItem = indexPath.row
+		
+		}
+}
