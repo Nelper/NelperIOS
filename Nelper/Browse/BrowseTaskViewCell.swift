40006096656dad0d85ecbcad2697414b16767f09
Pierre-Luc Benoit <plucbenoit@gmail.com>
2015-11-08 02:47:42 -0500

    browse applications

--- Nelper/Browse/BrowseTaskViewCell.swift
+++ Nelper/Browse/BrowseTaskViewCell.swift
@@ -24,6 +24,7 @@ class BrowseTaskViewCell: UITableViewCell {
 	var task: Task!
 	var cellIndexPath: NSIndexPath!
 	var moneyContainer: UIView!
+	var appliedNotice: UILabel!
 	
 	//MARK: Initialization
 	
@@ -44,9 +45,7 @@ class BrowseTaskViewCell: UITableViewCell {
 		self.picture.layer.masksToBounds = true
 		self.picture.clipsToBounds = true
 		self.picture.contentMode = UIViewContentMode.ScaleAspectFill
-		
 		cellView.addSubview(picture)
-		
 		picture.snp_makeConstraints { (make) -> Void in
 			make.left.equalTo(cellView.snp_left).offset(10)
 			make.centerY.equalTo(cellView.snp_centerY)
@@ -58,9 +57,7 @@ class BrowseTaskViewCell: UITableViewCell {
 		self.categoryPicture = categoryPicture
 		self.categoryPicture.layer.cornerRadius = self.categoryPicture.frame.size.width / 2
 		self.categoryPicture.clipsToBounds = true
-		
 		cellView.addSubview(categoryPicture)
-	
 		categoryPicture.snp_makeConstraints { (make) -> Void in
 			make.bottom.equalTo(picture.snp_bottom)
 			make.left.equalTo(picture.snp_right).offset(-25)
@@ -77,20 +74,9 @@ class BrowseTaskViewCell: UITableViewCell {
 			make.right.equalTo(cellView.snp_right).offset(-10)
 		}
 		
-		/*let authorIcon = UIImageView()
-		cellView.addSubview(authorIcon)
-		authorIcon.image = UIImage(named: "profile-red")
-		authorIcon.snp_makeConstraints { (make) -> Void in
-			make.left.equalTo(picture.snp_right).offset(22)
-			make.top.equalTo(title.snp_bottom).offset(5)
-			make.width.equalTo(20)
-			make.height.equalTo(20)
-		}*/
-		
 		let author = UILabel()
 		self.author = author
 		cellView.addSubview(author)
-		
 		author.snp_makeConstraints { (make) -> Void in
 			make.top.equalTo(title.snp_bottom).offset(8)
 			make.left.equalTo(title.snp_left)
@@ -103,7 +89,6 @@ class BrowseTaskViewCell: UITableViewCell {
 		self.creationDate.font = UIFont(name: "Lato-Light", size: kText13)
 		self.creationDate.textColor = blackTextColor
 		
-		
 		let moneyContainer = UIView()
 		self.moneyContainer = moneyContainer
 		cellView.addSubview(moneyContainer)
@@ -111,13 +96,13 @@ class BrowseTaskViewCell: UITableViewCell {
 		moneyContainer.layer.cornerRadius = 3
 		moneyContainer.snp_makeConstraints { (make) -> Void in
 			make.right.equalTo(cellView.snp_right).offset(-20)
-			make.bottom.equalTo(creationDate.snp_bottom).offset(-2)
+			make.centerY.equalTo(picture.snp_centerY).offset(pictureSize / 5)
 			make.width.equalTo(55)
 			make.height.equalTo(35)
 		}
 		
 		creationDate.snp_makeConstraints { (make) -> Void in
-			make.top.equalTo(author.snp_bottom).offset(5)
+			make.top.equalTo(author.snp_bottom).offset(4)
 			make.left.equalTo(author.snp_left)
 			make.right.equalTo(moneyContainer.snp_left).offset(6)
 		}
@@ -130,6 +115,17 @@ class BrowseTaskViewCell: UITableViewCell {
 		moneyLabel.font = UIFont(name: "Lato-Light", size: kText15)
 		moneyLabel.snp_makeConstraints { (make) -> Void in
 			make.edges.equalTo(moneyContainer.snp_edges)
+		}
+		
+		let appliedNotice = UILabel()
+		self.appliedNotice = appliedNotice
+		cellView.addSubview(appliedNotice)
+		appliedNotice.text = "Applied"
+		appliedNotice.font = UIFont(name: "Lato-Regular", size: kText15)
+		appliedNotice.textColor = redPrimary
+		appliedNotice.snp_makeConstraints { (make) -> Void in
+			make.centerX.equalTo(moneyContainer.snp_centerX)
+			make.centerY.equalTo(title.snp_centerY)
 		}
 		
 		/*let rightArrow = UIImageView()
@@ -155,9 +151,6 @@ class BrowseTaskViewCell: UITableViewCell {
 		
 		self.addSubview(cellView)
 	}
-	
-	
-	
 	
 	required init?(coder aDecoder: NSCoder) {
 		super.init(coder: aDecoder)
@@ -189,19 +182,29 @@ class BrowseTaskViewCell: UITableViewCell {
 		self.title.font = UIFont(name: "Lato-Regular", size: kText15)
 		self.title.textColor = blackTextColor
 		
-		self.author.text = "\(task.user.name)"
-		self.author.font = UIFont(name: "Lato-Light", size: kText13)
-		self.author.textColor = blackTextColor
-		
 		let price = String(format: "%.0f", task.priceOffered!)
 		
 		self.price.text = price+"$"
+		
+		self.author.text = "\(task.user.name)"
+		self.author.font = UIFont(name: "Lato-Light", size: kText13)
+		self.author.textColor = blackTextColor
 		
 		let dateHelper = DateHelper()
 		self.creationDate.text = "Posted \(dateHelper.timeAgoSinceDate(self.task.createdAt!, numericDates: true))"
 		
 		self.categoryPicture.image = UIImage(named: task.category!)
 		
+		if self.task.application != nil && self.task.application!.state != .Canceled {
+			self.appliedNotice.hidden = false
+			self.title.snp_remakeConstraints(closure: { (make) -> Void in
+				make.top.equalTo(self.picture.snp_top).offset(2)
+				make.left.equalTo(self.picture.snp_right).offset(18)
+				make.right.equalTo(self.appliedNotice.snp_left).offset(-10)
+			})
+		} else {
+			self.appliedNotice.hidden = true
+		}
 	}
 	
 	/**
